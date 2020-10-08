b53caa1345151c9b337b6e71f7ab0edb7fe27ccd
darker-space <47153177+darker-space@users.noreply.github.com>
2020-10-08 21:33:03 +0530

    Update connection.js

--- DB/connection.js
+++ DB/connection.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 
-var URI = "mongodb+srv://Aakash:aakash@aakash.suijx.mongodb.net/Aakash?retryWrites=true&w=majority";
+var URI = "mongodb+srv://Aakash:username@password.suijx.mongodb.net/DBname?retryWrites=true&w=majority";
 
 const connectDB = async() => {
     await mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true });
